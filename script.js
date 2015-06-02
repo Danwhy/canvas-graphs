9ff1cc6b606e51202e12087a99b289ddc6234c39
Danielwhyte <danwhy@gmail.com>
2015-06-02 09:44:28 +0100

    fixed spacing issue

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var canvas = document.getElementById('canvas'),
     highest = 0;
 
 var stats = {
-    one: 2,
-    two : 10,
+    word1: 2,
+    wordtwo : 10,
     three: 5,
     four : 7,
     five: 15,
@@ -24,7 +24,7 @@ for (var datum in stats){
 var options = {
     barColor: 'red',
     barSpacing: 0.5,
-    barWidth: 30,
+    barWidth: 60,
     graphHeight: 300,
     graphWidth: 'auto',
 };
@@ -55,7 +55,8 @@ var fillGraph = function(){
         ctx.translate(canvas.width / 2,canvas.height / 2);
         ctx.rotate(270 * Math.PI / 180);
         ctx.font = "20px sans-serif";
-        ctx.fillText(word, -ctx.measureText(word).width - 10, -(canvas.width / 2) + i + 20 + options.barWidth);
+        // FIXED?
+        ctx.fillText(word, -ctx.measureText(word).width - 10, -(canvas.width / 2) + 50 + (options.barWidth * options.barSpacing) + i);
         ctx.restore();
         ctx.fillStyle = options.barColor;
         ctx.fillRect(15 + (options.barWidth * options.barSpacing) + i, canvas.height / 2, options.barWidth, -((options.graphHeight / highest) * stats[word]));
